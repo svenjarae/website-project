e0701d9bf342226796739ee94b0d7586e21c280d
svenjarae <50318129+svenjarae@users.noreply.github.com>
2023-12-25 22:05:21 +0000

    added subdomain

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/deine-subdomain/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/projects/' : '/',
   pluginOptions: {
     i18n: {
       locale: 'en',
