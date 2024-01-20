57f8902cbd57bc6e0b8243b7655e9d32b53c8aa9
svenjarae <50318129+svenjarae@users.noreply.github.com>
2024-01-20 22:30:24 +0000

    added mobile styling: filter butns are now bottom right

--- src/components/AllProjectsComponent.vue
+++ src/components/AllProjectsComponent.vue
@@ -88,7 +88,7 @@ export default {
 }
 
 button {
-  margin-right: 10px;
+  margin-left: 10px;
     padding: 8px 12px;
     cursor: pointer;
     background-color: #333;
@@ -117,7 +117,8 @@ button {
     margin-left: 20px;
     position: fixed;
     z-index: 1;
-    bottom: 40px;
+    bottom: 0;
+    right: 20px;
 }
 }
 </style>
